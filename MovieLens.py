cff7f194a011d7c3b1e36f089b2fd96b2eba92a8
Tushar <garg.tusharmnnit@gmail.com>
2019-06-16 03:22:12 +0530

    First Commit, reading dataset

---
+++ MovieLens.py
@@ -0,0 +1,43 @@
+import os
+import csv
+import sys
+import re
+
+from surprise import Dataset
+from surprise import Reader
+
+from collections import defaultdict
+import numpy as np
+
+class MovieLens:
+
+    movieID_to_name = {}
+    name_to_movieID = {}
+    ratingsPath = '../ml-latest-small/ratings.csv'
+    moviesPath = '../ml-latest-small/movies.csv'
+    
+    def loadMovieLensLatestSmall(self):
+
+        # Look for files relative to the directory we are running from
+        os.chdir(os.path.dirname('C:/My Files/Projects/RecSys/RecSys-Materials/ContentBased'))
+
+        ratingsDataset = 0
+        self.movieID_to_name = {}
+        self.name_to_movieID = {}
+
+        reader = Reader(line_format='user item rating timestamp', sep=',', skip_lines=1)
+
+        ratingsDataset = Dataset.load_from_file(self.ratingsPath, reader=reader)
+
+        with open(self.moviesPath, newline='', encoding='ISO-8859-1') as csvfile:
+                movieReader = csv.reader(csvfile)
+                next(movieReader)  #Skip header line
+                for row in movieReader:
+                    movieID = int(row[0])
+                    movieName = row[1]
+                    self.movieID_to_name[movieID] = movieName
+                    self.name_to_movieID[movieName] = movieID
+
+        return ratingsDataset
+
+    
