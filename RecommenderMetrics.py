7b0b1c5dbef213df917eff2bc2186af7aedb5cea
Tushar <garg.tusharmnnit@gmail.com>
2019-06-19 01:38:08 +0530

    Evaluation Metrics, MAE,RMSE, Hit Rate,C.Hit Rate

---
+++ RecommenderMetrics.py
@@ -0,0 +1,72 @@
+import itertools
+
+from surprise import accuracy
+from collections import defaultdict
+
+class RecommenderMetrics:
+
+    def MAE(predictions):
+        return accuracy.mae(predictions, verbose=False)
+
+    def RMSE(predictions):
+        return accuracy.rmse(predictions, verbose=False)
+
+    def GetTopN(predictions, n=50, minimumRating=2.5):
+        topN = defaultdict(list)
+
+
+        for userID, movieID, actualRating, estimatedRating, _ in predictions:
+            if (estimatedRating >= minimumRating):
+                topN[int(userID)].append((int(movieID), estimatedRating))
+
+        for userID, ratings in topN.items():
+            ratings.sort(key=lambda x: x[1], reverse=True)
+            topN[int(userID)] = ratings[:n]
+
+        return topN
+
+    def HitRate(topNPredicted, leftOutPredictions):
+        hits = 0
+        total = 0
+
+        # For each left-out rating
+        for leftOut in leftOutPredictions:
+            userID = leftOut[0]
+            leftOutMovieID = leftOut[1]
+            # Is it in the predicted top 10 for this user?
+            hit = False
+            for movieID, predictedRating in topNPredicted[int(userID)]:
+                if (int(leftOutMovieID) == int(movieID)):
+                    hit = True
+                    break
+            if (hit) :
+                hits += 1
+
+            total += 1
+
+        # Compute overall precision
+        return hits/total
+
+    def CumulativeHitRate(topNPredicted, leftOutPredictions, ratingCutoff=0):
+        hits = 0
+        total = 0
+
+        # For each left-out rating
+        for userID, leftOutMovieID, actualRating, estimatedRating, _ in leftOutPredictions:
+            # Only look at ability to recommend things the users actually liked...
+            if (actualRating >= ratingCutoff):
+                # Is it in the predicted top 10 for this user?
+                hit = False
+                for movieID, predictedRating in topNPredicted[int(userID)]:
+                    if (int(leftOutMovieID) == movieID):
+                        hit = True
+                        break
+                if (hit) :
+                    hits += 1
+
+                total += 1
+
+        # Compute overall precision
+        return hits/total
+
+   
